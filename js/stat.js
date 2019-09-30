98520d575084a9b286f8ef638b7391d044a8d90f
Timofeev Ivan <ivant87@mail.ru>
2019-09-30 22:48:03 +0300

    сделал что-то

--- js/stat.js
+++ js/stat.js
@@ -1,10 +1,30 @@
-var canvas = document.createElement('canvas')
-var ctx = canvas.getContext('2d');
-canvas.width = 420;
-canvas.height = 270;
-ctx.fillRect(100, 10, 420, 270);
-document.body.appendChild(canvas);
+var CLOUD_WIDTH = 420;
+var CLOUD_HEIGHT = 270;
 
+var CLOUD_X = 100;
+var CLOUD_Y = 10;
+var GAP = 50;
+var FONT_GAP = 50;
+var TEXT_WIDTH = 50;
+var BAR_HEIGHT = 150;
+var barWidth = 40;
 
+var renderCloud = function(ctx, x, y, color) {
+    ctx.fillStyle = color;
+    ctx.fillRect(x, y, CLOUD_WIDTH, CLOUD_HEIGHT);
+};
 
-var renderStatistics = function(ctx, names, times) {};
+window.renderStatistics = function(ctx) {
+    renderCloud(ctx, 110, 20, 'rgba(0, 0, 0, 0.7)');
+    renderCloud(ctx, 100, 10, 'white');
+
+    ctx.fillStyle = 'black';
+    ctx.font = '16px PT Mono';
+    ctx.textBaseline = 'hanging';
+    ctx.fillText('Ура вы победили!', 120, 30);
+    ctx.fillText('Список результатов:', 120, 50);
+
+    ctx.fillStyle = 'rgba(255, 0, 0, 1)';
+    ctx.fillText('Ivan', 140, 250);
+    ctx.fillRect(140, 70, 50, 150);
+};
